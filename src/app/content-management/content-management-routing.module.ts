85929df6f178f38556744c8cbc725b3dd7d4b80b
shivam17acelucid <sahibkhan863075@gmail.com>
2022-10-27 15:20:47 +0530

    some minor fixes

--- src/app/content-management/content-management-routing.module.ts
+++ src/app/content-management/content-management-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
     path: 'editsubcategory', component: AddEditSubcategoryComponent
   },
   {
-    path: 'addsponsor', component: AddEditSponsorComponent
+    path: 'editsponsor', component: AddEditSponsorComponent
   },
   {
     path:'addcategory', component: AddEditCategoryComponent
