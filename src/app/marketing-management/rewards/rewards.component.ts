775051527020509da27ec700d393a12c17f71813
shivam17acelucid <sahibkhan863075@gmail.com>
2022-12-07 14:03:02 +0530

    added reward redemption table and fixed table layout globally

--- src/app/marketing-management/rewards/rewards.component.ts
+++ src/app/marketing-management/rewards/rewards.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { rewardsRedemptionData } from 'src/app/DummyData/marketing';
+import rewards from 'src/app/_models/marketingModule';
+import { TABLE_HEADING } from 'src/app/_models/table_heading';
+import { MarketingService } from 'src/app/_services/marketing';
+import { NgxUiLoaderService, SPINNER } from 'ngx-ui-loader';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-rewards',
@@ -6,11 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rewards.component.scss']
 })
 export class RewardsComponent implements OnInit {
+  @ViewChild('dt') dt: Table | undefined;
   sidebarSpacing: string;
-  constructor() { }
+  rewardsRedemptionData: rewards[] = [];
+  fgsType: any;
+  cols!: TABLE_HEADING[];
+
+  constructor(
+    private ngxLoader: NgxUiLoaderService,
+    private MarketingService: MarketingService
+  ) { }
 
   ngOnInit(): void {
     this.sidebarSpacing = 'contracted';
+    this.fgsType = SPINNER.squareLoader
+    this.ngxLoader.start();
+    this.sidebarSpacing = 'contracted';
+    this.getRewardRedemption()
+    this.cols = [
+      { field: 'couponCodeId', show: true, headers: 'Coupon Id' },
+      { field: 'couponCodeName', show: true, headers: 'Coupon Name' },
+      { field: 'customerEmail', show: true, headers: 'Customer Email' },
+      { field: 'appliedOn', show: true, headers: 'Applied On' },
+      { field: 'completedOn', show: true, headers: 'Completed On' },
+      { field: 'status', show: true, headers: 'Status' },
+    ]
   }
 
   onToggleSidebar(sidebarState: any) {
@@ -19,5 +45,16 @@ export class RewardsComponent implements OnInit {
     } else {
       this.sidebarSpacing = 'expanded';
     }
+  }
+
+  getRewardRedemption(){
+    this.MarketingService.getRewardRedemptionList().subscribe((data => {
+      this.rewardsRedemptionData = data
+    }))
+    console.log(rewardsRedemptionData);
+  }
+  applyFilterGlobal($event, stringVal) {
+    this.dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+    console.log($event)
   }
 }
