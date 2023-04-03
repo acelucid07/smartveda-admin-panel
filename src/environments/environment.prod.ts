ee0a0aeeca5866c8a6e1b2555025ca09923bc0b0
vipin <vipin.badola@acelucid.com>
2023-04-03 14:16:45 +0530

    new build changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   BASE_URL:"http://13.126.212.31:5000",
-  JSON_SERVER:" http://localhost:3000"
+  JSON_SERVER:" http://localhost:3000",
+  IP_ADDRESS:"http://13.126.212.31"
 };
